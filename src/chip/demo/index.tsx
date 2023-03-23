e383f49ba7060a93ba84abd53cbb7488110d5586
sdhushu <1426732052@qq.com>
2023-03-23 13:46:21 +0800

    fix: add chip style config

--- src/chip/demo/index.tsx
+++ src/chip/demo/index.tsx
@@ -4,13 +4,52 @@ import React from 'react';
 const Demo = () => {
   return (
     <>
-      <AppBar title={'纸片类型'}>
+      <AppBar title="纸片类型">
         <Chip>默认纸片</Chip>
         <Chip type="primary">主要纸片</Chip>
         <Chip type="success">成功纸片</Chip>
         <Chip type="danger">危险纸片</Chip>
         <Chip type="warning">警告纸片</Chip>
         <Chip type="info">信息纸片</Chip>
+      </AppBar>
+      <AppBar title="空心纸片">
+        <Chip plain type="primary">
+          空心纸片
+        </Chip>
+      </AppBar>
+      <AppBar title="非圆角纸片">
+        <Chip round={false} type="primary">
+          非圆角纸片
+        </Chip>
+      </AppBar>
+      <AppBar title="纸片大小">
+        <Chip type="primary">常规纸片</Chip>
+        <Chip type="success" size="small">
+          小型纸片
+        </Chip>
+        <Chip type="warning" size="mini">
+          迷你纸片
+        </Chip>
+        <Chip type="danger" size="large">
+          大型纸片
+        </Chip>
+      </AppBar>
+      <AppBar title="块级纸片">
+        <Chip block type="primary">
+          块级纸片
+        </Chip>
+      </AppBar>
+      <AppBar title="自定义颜色">
+        <Chip color="#009688">纸片</Chip>
+        <Chip color="#009688" plain>
+          纸片
+        </Chip>
+        <Chip color="#faecd8" text-color="#e6a23c" plain>
+          纸片
+        </Chip>
+        <Chip color="#faecd8" text-color="#e6a23c">
+          纸片
+        </Chip>
       </AppBar>
     </>
   );
--- src/chip/index.tsx
+++ src/chip/index.tsx
@@ -1,14 +1,41 @@
 import { ChipProps, defaultProps } from 'asap-ui/chip/props';
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { createNameSpace } from '../utils/components';
 import './index.less';
 
 const Chip: FC<ChipProps> = (p) => {
   const props = { ...defaultProps, ...p };
   const { n, classes } = createNameSpace('chip');
+
+  const chipStyle = useMemo(() => {
+    const { color, textColor, plain } = props;
+    if (plain)
+      return {
+        borderColor: color,
+        color: textColor || color,
+      };
+    return {
+      backgroundColor: color,
+      color: textColor,
+    };
+  }, [props]);
+
+  const contentClass = useMemo(() => {
+    const { size, block, type, plain, round } = props;
+    const blockClass = block ? n('$--flex') : n('$--inline-flex');
+    const plainTypeClass = plain
+      ? `${n('plain')} ${n(`plain-${type}`)}`
+      : n(`--${type}`);
+    const roundClass = round ? n('--round') : null;
+    return [n(`--${size}`), blockClass, plainTypeClass, roundClass];
+  }, [props]);
+
   return (
     <>
-      <span className={classes(n(), n('$--box'))}>
+      <span
+        className={classes(n(), n('$--box'), ...contentClass)}
+        style={chipStyle}
+      >
         <span className={n(`test-${props.size}`)}>{props.children}</span>
       </span>
     </>
