15ba7dfd1fbabe4aa50ffdc0c764efbf1262b9f3
sdhushu <1426732052@qq.com>
2023-04-19 13:38:02 +0800

    fix: input label transform

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'asap-ui/utils/hooks';
 import React, { type FC } from 'react';
 import '../styles/common.less';
 import { createNameSpace } from '../utils/components';
@@ -7,11 +8,25 @@ import { defaultProps, InputProps } from './props';
 const Input: FC<InputProps> = (p) => {
   const { n, classes } = createNameSpace('input');
   const props = { ...defaultProps, ...p };
+  const [focus, setFocus] = useState(false);
+
+  const handleFocus = () => {
+    setFocus(true);
+  };
+
+  const handleBlur = () => {
+    setFocus(false);
+  };
 
   return (
-    <div className={classes(n(), n('$--box'))}>
-      <input className={n('input')} />
-      <label className={classes(n('placeholder'), n('$--ellipsis'))}>
+    <div className={classes(n(), n('$--box'), [focus, n('active')])}>
+      <input className={n('input')} onFocus={handleFocus} onBlur={handleBlur} />
+      <label
+        className={classes(n('placeholder'), n('$--ellipsis'), [
+          focus,
+          n('focus'),
+        ])}
+      >
         {props.placeholder}
       </label>
     </div>
